63099b984bcdc8f3f3cebca9e4863497c2b1f3cf
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-07-31 07:02:42 +0500

    sidebar fully working

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,26 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
-  activeLink !: string;
-
-  constructor(private router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.activeLink = event.urlAfterRedirects.split('/')[1];
-      }
-    });
-  }
-
-  setActiveLink(link: string) {
-    this.activeLink = link;
-  }
-}
+export class SidebarComponent {}
