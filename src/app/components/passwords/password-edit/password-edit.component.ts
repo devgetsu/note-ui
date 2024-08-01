1666c348101af94953b5ff245d8045a0ce562a8e
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-08-01 21:38:42 +0500

    щл ш ыуте увше

--- src/app/components/passwords/password-edit/password-edit.component.ts
+++ src/app/components/passwords/password-edit/password-edit.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../../services/project-service/project.service';
 import { PasswordService } from '../../../services/password-service/password.service';
 import { UpdatePasswordModel } from '../../../interfaces/password-interfaces/update-password';
+import { FormsModule } from '@angular/forms';
+import { ProjectModel } from '../../../interfaces/project-interfaces/get-pr';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-password-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './password-edit.component.html',
   styleUrl: './password-edit.component.scss'
 })
-export class PasswordEditComponent {
-  constructor(private _projectService : ProjectService, private _passwordService : PasswordService) {
+export class PasswordEditComponent implements OnInit{
+  constructor(private _projectService : ProjectService, private _passwordService : PasswordService, private route: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.changeIdUpdate();
+    this.getAllProjects();
+    this.getPasswordById(this.updateModel.id);
   }
 
   getPasswordById(id:string){
     this._passwordService.getPasswordById(id).subscribe(
       (data)=>{
-        console.log(data)
+        this.updateModel.login = data.login
+        this.updateModel.pass = data.pass
+        this.updateModel.program = data.program
+        this.updateModel.projectId = data.projectId
       }
     )
   }
@@ -29,9 +40,27 @@ export class PasswordEditComponent {
     pass: '',
     projectId: ''
   }
-  changeIdUpdate(id:string) {
-    this.updateModel.id = id;
+  
+  changeIdUpdate() {
+    this.route.paramMap.subscribe(params => {
+      this.updateModel.id != params.get('id');
+    });
   }
 
+  projects !: ProjectModel[];
+  
+  getAllProjects(){
+    this._projectService.getProject().subscribe(
+      (data)=>{
+        this.projects = data;
+      }
+    )
+  }
 
+  updatePassword(){
+    this._passwordService.updatePassword(this.updateModel).subscribe(
+      (data)=>{
+        console.log(data);
+      })
+  }
 }
