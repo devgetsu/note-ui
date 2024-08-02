06b7494725fbc69e2a917860a1bc416cb81d9906
AcciGen <145344775+AcciGen@users.noreply.github.com>
2024-08-02 19:53:29 +0500

    some fixes and adding some responsive counts to main page

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,18 +3,40 @@ import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../../services/project-service/project.service';
 import { ProjectModel } from '../../interfaces/project-interfaces/get-pr';
 import { DeleteProjectModel } from '../../interfaces/project-interfaces/delete-pr';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent implements OnInit {
+  
   constructor(private _projectService : ProjectService, private _router : Router){}
+
   ngOnInit(): void {
     this.getAllProjects();
+  }
+
+  cardData: any = {
+    projectMoney: 0,
+    targetAmount: 0,
+    receivedMoney: 0,
+    remainingMoney: 0,
+    salaryEmployee: 0,
+    fines: 0,
+    remainingSalary: 0,
+    expenses: 0,
+    projectsCount: 0,
+    employeesCount: 0,
+    salaryPercentage: 0
+  };
+
+
+  private sumField(data: any[], field: string): number {
+    return data.reduce((sum, item) => sum + (item[field] || 0), 0);
   }
 
   projects !: ProjectModel[];
@@ -27,6 +49,12 @@ export class MainComponent implements OnInit {
     this._projectService.getProject().subscribe(
       (data)=>{
         this.projects = data;
+
+        this.cardData.projectMoney = this.sumField(data, 'amount')
+        this.cardData.targetAmount = this.sumField(data, 'budgetTarget')
+        this.cardData.receivedMoney = this.sumField(data, 'received')
+        this.cardData.remainingMoney = this.cardData.projectMoney - this.cardData.receivedMoney
+        this.cardData.projectsCount = data.length;
       }
     )
   }
